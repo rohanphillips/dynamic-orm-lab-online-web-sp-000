6167e7d52acefba743ca1d6fb6b63b1979a99b91
Rohan Phillips <rohan@solutions-insurance.com>
2019-08-21 22:23:50 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -3,11 +3,11 @@ require 'active_support/inflector'
 require 'interactive_record.rb'
 
 class Student < InteractiveRecord
-  self.column_names.each do |col_name|
+  column_names.each do |col_name|
     attr_accessor col_name.to_sym
   end
 
   def initialize(info_hash = {})
-    info_hash.each{|key, value| self.send(("#{key}="), value)}
+    info_hash.each{|key, value| self.send("#{key}=", value)}
   end
 end
